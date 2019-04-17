084b7306da828e6181dcaef70fa48b6dfec2bb35
Valencia <davidalencia@gmail.com>
2019-04-16 22:56:01 -0500

    ahora si colas ya esta  | 22:56 04/16/19

---
+++ src/main/java/mx/unam/ciencias/edd/proyecto2/svg/Flecha.java
@@ -0,0 +1,72 @@
+package mx.unam.ciencias.edd.proyecto2.svg;
+
+public class Flecha extends SVG {
+
+  private boolean cabezera;
+
+  //marker-end="url(#end-arrow)" marker-start="url(#start-arrow)"
+  public Flecha(String x1, String y1, String x2, String y2, String stroke){
+    super("line");
+    cabezera = true;
+    setAtributo("x1", x1);
+    setAtributo("y1", y1);
+    setAtributo("x2", x2);
+    setAtributo("y2", y2);
+    setAtributo("stroke", stroke);
+    setAtributo("marker-end", "url(#end-arrow)");
+  }
+  public Flecha(Integer x1, Integer y1, Integer x2, Integer y2, String stroke){
+    super("line");
+    cabezera = true;
+    setAtributo("x1", x1.toString());
+    setAtributo("y1", y1.toString());
+    setAtributo("x2", x2.toString());
+    setAtributo("y2", y2.toString());
+    setAtributo("stroke", stroke);
+    setAtributo("marker-end", "url(#end-arrow)");
+  }
+  public Flecha(String x1, String y1, String x2, String y2){
+    super("line");
+    cabezera = true;
+    setAtributo("x1", x1);
+    setAtributo("y1", y1);
+    setAtributo("x2", x2);
+    setAtributo("y2", y2);
+    setAtributo("stroke", "black");
+    setAtributo("marker-end", "url(#end-arrow)");
+  }
+  public Flecha(Integer x1, Integer y1, Integer x2, Integer y2){
+    super("line");
+    cabezera = true;
+    setAtributo("x1", x1.toString());
+    setAtributo("y1", y1.toString());
+    setAtributo("x2", x2.toString());
+    setAtributo("y2", y2.toString());
+    setAtributo("stroke", "black");
+    setAtributo("marker-end", "url(#end-arrow)");
+  }
+
+  public static SVG headers(){
+    SVG r = new SVG("defs");
+    String s = "<marker id='end-arrow' markerWidth='10' markerHeight='10' refX='0' refY='5' orient='auto' markerUnits='strokeWidth'>\n"+
+      "<path d='M0,0 L0,10 L5,5 z' fill='black' />\n"+
+      "</marker>\n";
+    r.setContenido(s);
+    return r;
+  }
+  public void setCabezera(boolean cabezera){
+    this.cabezera = cabezera;
+  }
+  @Override public String toString(){
+    String s ="";
+    if(cabezera){
+      s = "<defs>\n"+
+      "<marker id='end-arrow' markerWidth='10' markerHeight='10' refX='0' refY='5' orient='auto' markerUnits='strokeWidth'>\n"+
+        "<path d='M0,0 L0,10 L5,5 z' fill='black' />\n"+
+        "</marker>\n"+
+      "</defs>\n";
+    }
+    s += super.toString();
+    return s;
+  }
+}
--- src/main/java/mx/unam/ciencias/edd/proyecto2/svg/PilaSVG.java
+++ src/main/java/mx/unam/ciencias/edd/proyecto2/svg/PilaSVG.java
@@ -3,27 +3,27 @@ package mx.unam.ciencias.edd.proyecto2.svg;
 import mx.unam.ciencias.edd.Pila;
 import java.util.Iterator;
 
-public class PilaSVG<T> extends Pila<T> implements toSVG<T> {
+public class PilaSVG<T> /*extends Pila<T> implements toSVG<T>*/ {
 
-  private class Iterador implements Iterator<T> {
-
-      private Nodo n;
-
-      /* Inicializa al iterador. */
-      public Iterador() {
-        n = cabeza;
-      }
-
-      /* Nos dice si hay un elemento siguiente. */
-      @Override public boolean hasNext() {
-          return n!=null && n.siguiente !=null;
-      }
-
-      /* Regresa el siguiente elemento en orden BFS. */
-      @Override public T next() {
-          n = n.siguiente;
-          return n.elemento;
-      }
-  }
+  // private class Iterador implements Iterator<T> {
+  //
+  //     private Nodo n;
+  //
+  //     /* Inicializa al iterador. */
+  //     public Iterador() {
+  //       n = cabeza;
+  //     }
+  //
+  //     /* Nos dice si hay un elemento siguiente. */
+  //     @Override public boolean hasNext() {
+  //         return n!=null && n.siguiente !=null;
+  //     }
+  //
+  //     /* Regresa el siguiente elemento en orden BFS. */
+  //     @Override public T next() {
+  //         n = n.siguiente;
+  //         return n.elemento;
+  //     }
+  // }
 
 }
